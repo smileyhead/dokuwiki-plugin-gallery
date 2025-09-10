b7b3420561d926f96ab37d0afe9841838f219b67
smileyhead <szijartoleventepal@gmail.com>
2025-09-10 19:13:05 +0200

    Create 'hu-informal' translation

---
+++ lang/hu-informal/lang.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+ * @license    GPL 2 (http://www.gnu.org/licenses/gpl.html)
+ * 
+ * @author DelDadam <deldadam@gmail.com>
+ * @author Szíjártó Levente Pál <szijartoleventepal@gmail.com>
+ */
+$lang['pages']                 = 'Képgaléria oldalai:';
+$lang['js']['addgal']          = 'Névtér hozzáadása galériaként';
+$lang['nothingfound'] = 'Nem található kép.';
+$lang['fail'] = 'A galéria betöltése meghiúsult';
+
+
+$lang['js']['label_toolbar_button'] = 'Galéria beszúrása';
+$lang['js']['title_dialog'] = 'Galéria bővítmény';
+$lang['js']['label_namespace'] = 'Névtér vagy RSS-hírfolyam';
+$lang['js']['label_thumbnailsize'] = 'Bélyegképek mérete';
+$lang['js']['pattern_hint_thumbnailsize'] = '<number><lowercase x><number>';
+$lang['js']['label_imagesize'] = 'A hivatkozott képek mérete közvetlen módban.';
+$lang['js']['pattern_hint_imagesize'] = '<number><uppercase X><number>';
+$lang['js']['label_cache'] = 'Bélyegképek gyorsítótárazása';
+$lang['js']['label_filter'] = 'Szűrő (tartalmaznia kell egy „*” karaktert)';
+$lang['js']['label_showname'] = 'Fájlnév mutatása a bélyegképek alatt';
+$lang['js']['label_showtitle'] = '„Headline” EXIF-címke mutatása címként a bélyegképek alatt';
+$lang['js']['label_crop'] = 'Bélyegképek átméretezése (és szükség esetén levágása) a megadott méretre';
+$lang['js']['label_direct'] = 'Képek nagyítása kattintásra, a részleteket megjelenítő oldal megnyitása helyett';
+$lang['js']['label_lightbox'] = 'Képek megjelenítése egy jól mutató JavaScript-ablakban';
+$lang['js']['label_reverse'] = 'Képek megjelenítési sorrendjének megfordítása';
+$lang['js']['label_recursive'] = 'Képek keresése a megadott névtér alnévtereiben is';
+$lang['js']['label_align_left'] = 'balra igazítás';
+$lang['js']['label_align_center'] = 'középre igazítás';
+$lang['js']['label_align_right'] = 'jobbra igazítás';
+$lang['js']['label_cols'] = 'A rácsos elrendezés oszlopainak száma';
+$lang['js']['label_limit'] = 'Megjelenített képek számának korlátozása. „0” esetén mind megjelenik.';
+$lang['js']['label_offset'] = 'Az első x kép elrejtése. Együtt használható az előző lehetőséggel.';
+$lang['js']['label_paginate'] = 'Galéria oldalakra bontása, oldalanként a megadott képmennyiséggel. „0” esetén nincs szétbontás.';
+$lang['js']['label_sort_file'] = 'Képek rendezése fájlnév szerint';
+$lang['js']['label_sort_random'] = 'Képek rendezése véletlenszerűen. Ehhez a lehetőséghez javasolt a gyorsítótár letiltása.';
+$lang['js']['label_sort_mod'] = 'Képek rendezése legutóbbi módosítás szerint';
+$lang['js']['label_sort_exif_date'] = 'Képek rendezése EXIF-dátum szerint';
+$lang['js']['label_sort_exif_title'] = 'Képek rendezése EXIF-cím szerint';
